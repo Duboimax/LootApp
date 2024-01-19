b7825e2fd9d74fed1b3980ebc73dc2763715ff4c
duboimax <maxencedubois22@gmail.com>
2024-01-19 10:34:13 +0100

    first commit

---
+++ LootApp/LootAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LootAppApp.swift
+//  LootApp
+//
+//  Created by Maxence DUBOIS on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct LootAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
