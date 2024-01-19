040ea5ed158ff77c2650226666a34d14f6d027fa
duboimax <maxencedubois22@gmail.com>
2024-01-19 17:23:17 +0100

    pas mal de truc

---
+++ LootApp/Game.swift
@@ -0,0 +1,8 @@
+//
+//  Game.swift
+//  LootApp
+//
+//  Created by Maxence DUBOIS on 1/19/24.
+//
+
+import Foundation
---
+++ LootApp/LootItem.swift
@@ -0,0 +1,8 @@
+//
+//  LootItem.swift
+//  LootApp
+//
+//  Created by Maxence DUBOIS on 1/19/24.
+//
+
+import Foundation
